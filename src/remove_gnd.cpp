c73d90786f0c91fa4646ce1e78f96573822ca1ab
Philipp Pelcz <uhmeu@student.kit.edu>
2019-05-08 18:43:31 +0200

    Removing the Ground works now. The Code could be cleaned up a bit!

--- src/remove_gnd.cpp
+++ src/remove_gnd.cpp
@@ -9,44 +9,52 @@
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 #include <pcl_ros/transforms.h>
+#include <pcl/common/transforms.h> 
 #include <tf2_ros/transform_listener.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/Twist.h>
 #include <tf2/convert.h>
+#include <tf2_ros/buffer.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
 
 
 #define GND_LEVEL (0.03)
 
 
 ros::Publisher pub;
+geometry_msgs::TransformStamped transformStamped;
+
 
 void cloud_cb (const Cloud_cptr& input_cloud)
 {    
-    /*
-    tf2_ros::Buffer tfBuffer;
-    tf2_ros::TransformListener tfListener(tfBuffer);
-    sensor_msgs::PointCloud2 cloud_in_tr;
     
-    geometry_msgs::TransformStamped transformStamped;
+    Cloud input_cloud_tr;
+    sensor_msgs::PointCloud2 sens_msg_input_cloud, sens_msg_input_cloud_tr;
+    
+    ROS_INFO("Tranform frame is %s und %s", transformStamped.header.frame_id.c_str(), transformStamped.child_frame_id.c_str());
+    
+    //Conversion from Cloud to sensor_msgs
+    pcl::toROSMsg(*input_cloud, sens_msg_input_cloud);     
+    
+    //Transformation
+    tf2::doTransform(sens_msg_input_cloud, sens_msg_input_cloud_tr, transformStamped);
+
+    //Conversion from sensor_msgs::PointCloud2 to Cloud
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(sens_msg_input_cloud_tr,pcl_pc2);
+    pcl::fromPCLPointCloud2(pcl_pc2, input_cloud_tr);
     
     
-    try {
-        transformStamped = tfBuffer.lookupTransform("base_link", "camera_link", ros::Time(0));
-        std::cout << transformStamped << std::endl;
-        tf2::doTransform(input_cloud, cloud_in_tr, transformStamped);
-    }
-    catch (tf2::TransformException &ex) {
-            ROS_WARN("%s", ex.what());
-    }    
-    */
+    //Delete the GroundPoints
     Indices object_indices;
-    for( size_t i = 0; i < input_cloud->size(); i++ ){
-    	float z = input_cloud->points[i].z;
+    for( size_t i = 0; i < input_cloud_tr.size(); i++ ){
+    	float z = input_cloud_tr.points[i].z;
         if( z > GND_LEVEL) {
             object_indices.push_back(i);
         }
     }
-    Cloud output(*input_cloud, object_indices );
+    Cloud output(input_cloud_tr, object_indices );
 
   // Publish the data.
   pub.publish (output);
@@ -57,6 +65,16 @@ int main (int argc, char** argv)
   // Initialize ROS
   ros::init (argc, argv, "remove_gnd");
   ros::NodeHandle nh;
+  
+  tf2_ros::Buffer tfBuffer;
+  tf2_ros::TransformListener tfListener(tfBuffer);
+  
+  
+  try {
+     transformStamped = tfBuffer.lookupTransform("base_link", "camera_depth_optical_frame", ros::Time(0), ros::Duration(2));
+  } catch (tf2::TransformException &ex) {
+        ROS_WARN("%s", ex.what());
+  }
 
   // Create a ROS subscriber for the input point cloud
   ros::Subscriber sub = nh.subscribe ("/camera/depth_registered/points", 1, cloud_cb);
